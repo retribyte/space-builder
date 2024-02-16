0e1d91454025642eeacd93186d9a5509b615e472
retribyte <car100@students.uwf.edu>
2024-02-15 23:05:38 -0600

    Layout adjustments, added new styles, cleaned up file structure, new image in main column

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+function collapseSidebar(whichSide) {
+    console.log(whichSide);
+    // const column = document.getElementById(`${whichSide}-column-content`);
+    // column.classList.toggle('d-none');
+}
