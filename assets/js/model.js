4967bd74935b17aa5cacea9e531cf28f5c01c308
retribyte <car100@students.uwf.edu>
2024-03-23 15:13:54 -0500

    Implemented factory functions for Planets, Moons, and Stars

--- assets/js/model.js
+++ assets/js/model.js
@@ -6,10 +6,65 @@ function createPlanet(name, size, distance, type) {
         size: size,
         distance: distance,
         type: type,
-        moons: []
+        moons: [],
+        addMoon: function (name, size, distance) {
+            this.moons.push(createMoon(name, size, distance));
+        }
     }
-    // Do something to 'save' this data
-    // ...
-    // Return a copy of planet
-    return JSON.parse(JSON.stringify(planet));
+    return planet;
+}
+
+function createMoon(name, size, distance) {
+    let moon = {
+        name: name,
+        size: size,
+        distance: distance
+    }
+    return moon;
+}
+
+function createStar(name, size) {
+    let star = {
+        name: name,
+        size: size,
+        planets: [],
+        addPlanet: function (name, size, distance, type) {
+            this.planets.push(createPlanet(name, size, distance, type));
+        }
+    }
+    return star;
+}
+
+function test() {
+    // Example of how this would be used
+    let sol = createStar('Sol', 696340);
+    sol.addPlanet('Earth', 6371, 1, 'Terrestrial');
+    console.log(sol)
+    sol.planets.forEach((planet) => {
+        console.log(sol.name + " had a planet named " + planet.name)
+    })
+
+    let earth = sol.planets[0];
+    console.log(earth);
+
+    console.log("Adding planets...")
+    earth.addMoon('Luna', 1737, 0.002569);
+    earth.addMoon('Cupid', 925, 0.001352);
+    earth.addMoon('Labumba', 5184, 0.006128);
+    console.log(earth);
+    earth.moons.forEach((moon) => {
+        console.log(earth.name + " had a moon named " + moon.name)
+    })
+
+    // console.log(createPlanet('Earth', 6371, 1, 'Terrestrial'));
+    // console.log(createMoon('Luna', 1737, 0.002569));
+    // console.log(createStar('Sol', 696340));
+
+    let galaxy = {
+        name: 'Milky Way',
+        type: 'spiral',
+        systems: []
+    }
+    galaxy.systems.push(sol)
+    console.log(JSON.stringify(galaxy));
 }
