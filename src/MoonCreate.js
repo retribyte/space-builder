f353e3d038daddc59acfe1539940af31c04995f7
retribyte <car100@students.uwf.edu>
2024-03-28 11:29:37 -0500

    Added new components

---
+++ src/MoonCreate.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function MoonCreate() {
+    return (
+        <div id="objectDetails">
+            <div id="inputContainer">
+                <label htmlFor="name-input">Name: </label>
+                <input type="text" id="name-input" className="form-control" placeholder="Europa" />
+            </div>
+            <div id="inputContainer">
+                <label htmlFor="size-input">Size: </label>
+                <input type="text" id="size-input" className="form-control" placeholder="6000" />
+                <span className="unit">km</span>
+            </div>
+            <div id="inputContainer">
+                <label htmlFor="distance-input">Distance to planet:</label>
+                <input type="text" id="distance-input" className="form-control" placeholder="2.53" />
+                <span className="unit">km</span>
+            </div>
+            <div id="inputContainer">
+                <label htmlFor="distance-input">Moon type:</label>
+                <select id="planetType" className="form-select ">
+                    <option value="terrestrial">Terrestrial</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default MoonCreate;
---
+++ src/PlanetCreate.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function PlanetCreate() {
+    return (
+        <div id="objectDetails">
+            <div id="inputContainer">
+                <label htmlFor="name-input">Name: </label>
+                <input type="text" id="name-input" className="form-control" placeholder="Earth" />
+            </div>
+            <div id="inputContainer">
+                <label htmlFor="size-input">Size: </label>
+                <input type="text" id="size-input" className="form-control" placeholder="6000" />
+                <span className="unit">km</span>
+            </div>
+            <div id="inputContainer">
+                <label htmlFor="distance-input">Distance to star: </label>
+                <input type="text" id="distance-input" className="form-control" placeholder="2.53" />
+                <span className="unit">AU</span>
+            </div>
+            <div id="inputContainer">
+                <label htmlFor="distance-input">Planet type:</label>
+                <select id="planetType" className="form-select ">
+                    <option value="gas">Gas</option>
+                    <option value="terrestrial">Terrestrial</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default PlanetCreate;
