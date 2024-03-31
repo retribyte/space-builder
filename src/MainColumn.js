308bf2cd9083d28f416b6b1c9e6e07a303b7d117
retribyte <car100@students.uwf.edu>
2024-03-31 17:34:09 -0500

    Restored images, some style adjustments

--- src/MainColumn.js
+++ src/MainColumn.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import placeholder from './assets/images/bad-solar-system-diagram.avif';
 
 function MainColumn(props) {
     return (
         <main className="col px-0 h-100" id="main-column">
-            <img className="image" src="./assets/images/bad-solar-system-diagram.avif" alt="Placeholder diagram of the Solar System" />
             <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
                 <p style={{width: "60%", color: 'white'}}>{JSON.stringify(props.data)}</p>
                 <p style={{minHeight: "1em", color: 'white'}}>{JSON.stringify(props.selectedObject)}</p>
             </div>
+            <img className="image" src={placeholder} alt="Placeholder diagram of the Solar System" />
         </main>
     );
 }
