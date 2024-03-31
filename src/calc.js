ae031b4c4f72a11a2b5dbe6d60b810e2b3dba415
ArkRoyalKai <jwvastola@gmail.com>
2024-03-30 22:17:46 -0500

    updated export functions

---
+++ src/calc.js
@@ -0,0 +1,32 @@
+ export default{calcMass, calcGravity, calcDayLength, calcYearLength};
+
+export function calcMass (planetData) {
+    let volume = .75 * 3.14 * ((planetData.size/2) ** 3);
+    let density = 0;
+    let earthMass = 5.97219 * (10 ** 24);
+    if (planetData.type === "Terrestrial") {
+        density = 5.55;
+    }
+    else if (planetData.type ==="Gas") {
+        density = .687;
+    }
+    
+    return ((density / volume) / earthMass) ;
+};
+
+export function calcGravity (planetData) {
+
+    let G = 6.67 * 10**-11
+    let mass = calcMass(planetData);
+
+
+    return( G * (mass / ((planetData.size/2) ** 2)));
+};
+
+export function calcDayLength (planetData) {
+    return (planetData.size / 6371);
+}
+
+export function calcYearLength (planetData) {
+    return ((planetData.distance / 1) * 365)
+}
--- src/navbar.js
+++ src/navbar.js
@@ -1,19 +1,16 @@
-import React from "react";
-import './assets/css/style.css';
+import React from 'react';
 
-
-function navBar() {
-return (
-    <nav id="top-navbar" className="row navbar navbar-expand navbar-dark bg-dark">
-                <ul className="navbar-nav collapse navbar-collapse">
-                    <img src="./assets/images/7547889.png" className="header-icon" alt="Space Builder logo" />
-                    <a className="navbar-brand title" href="#">Space Builder</a>
-                    <li className="nav-item active"><a className="nav-link" href="index.html">Home</a></li>
-                    <li className="nav-item"><a className="nav-link" href="about.html">About</a></li>
-                </ul>
-            </nav>
-
-);
+function Navbar() {
+    return (
+        <nav className="row navbar navbar-expand navbar-dark bg-dark">
+            <ul className="navbar-nav collapse navbar-collapse">
+                <img src="./assets/images/7547889.png" className="header-icon" alt="Space Builder logo" />
+                <a className="navbar-brand title" href="/">Space Builder</a>
+                <li className="nav-item active"><a className="nav-link" href="index.html">Home</a></li>
+                <li className="nav-item"><a className="nav-link" href="about.html">About</a></li>
+            </ul>
+        </nav>
+    );
 }
 
-export default navBar;
+export default Navbar;
