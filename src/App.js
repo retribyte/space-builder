b149be4156f40f63155d08bbc57e724097abdce8
retribyte <car100@students.uwf.edu>
2024-04-06 20:30:16 -0500

    Adjusted everything to work with one system at a time

--- src/App.js
+++ src/App.js
@@ -5,48 +5,11 @@ import ChildrenPanel from './ChildrenPanel';
 import MainColumn from './MainColumn';
 import InfoPanel from './InfoPanel';
 import factory from './factory';
+import SystemView from './SystemView';
 // import sampleData from './sample.json';
 
 function App() {
-    const [galaxy, setGalaxy] = useState({systems: []});
-    const [selectedObject, setSelectedObject] = useState(null);
-    
-    const [isChildrenPanelCollapsed, setChildrenPanelCollapsed] = useState(false);
-    const [isCreatePanelCollapsed, setCreatePanelCollapsed] = useState(true);
-    const [isInfoPanelCollapsed, setInfoPanelCollapsed] = useState(false);
-
-    const toggleChildrenPanel = () => {
-        setChildrenPanelCollapsed(!isChildrenPanelCollapsed)
-    }
-    const toggleCreatePanel = () => {
-        setCreatePanelCollapsed(!isCreatePanelCollapsed)
-    }
-    const toggleInfoPanel = () => {
-        setInfoPanelCollapsed(!isInfoPanelCollapsed)
-    }
-
-    function handleResize() {
-        console.log('Window resized; collapsing panels');
-        if (window.innerWidth <= 1260) {
-            if (!isChildrenPanelCollapsed) toggleChildrenPanel();
-            if (!isCreatePanelCollapsed) toggleCreatePanel();
-            if (!isInfoPanelCollapsed) toggleInfoPanel();
-        } else {
-            console.log('Window resized; expanding panels');
-            if (isChildrenPanelCollapsed) toggleChildrenPanel();
-            // The create panel remains collapsed unless explicitly opened by the user
-            if (isInfoPanelCollapsed) toggleInfoPanel();
-        }
-    };
-
-    useEffect(() => {
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, [isChildrenPanelCollapsed, isCreatePanelCollapsed, isInfoPanelCollapsed, toggleChildrenPanel, toggleCreatePanel, toggleInfoPanel]);
-
-    useEffect(() => {
-        handleResize();
-    }, []);
+    const [galaxy, setGalaxy] = useState({systems: [{}]});
 
     useEffect(() => {
         const savedData = localStorage.getItem('data');
@@ -63,67 +26,13 @@ function App() {
         }
     }, []);
 
-    const addNewChild = (details) => {
-        const {kind, primary, name, size, distance, objectCompositionType, temperature} = details;
-        if (kind == 'star') {
-            let star = factory.createStar(name, size, temperature);
-            const oldGalaxy = { ...galaxy };
-            let existingSystems = [];
-            if (Object.keys(oldGalaxy).length != 0) {
-                existingSystems = oldGalaxy.systems;
-            }
-            setGalaxy({
-                ...oldGalaxy,
-                systems: [
-                    ...existingSystems,
-                    star
-                ]
-            });
-        }
-        else if (kind == 'planet') {
-            factory.addPlanetToStar(setGalaxy, primary, name, size, distance, objectCompositionType);
-        }
-        else if (kind == 'moon') {
-            factory.addMoonToPlanet(setGalaxy, primary, name, size, distance, objectCompositionType);
-        }
-        else {
-            console.error('I don\'t know how to make a ' + kind + '...');
-        }
-
-        console.log(galaxy);
-    };
-
     useEffect(() => {
-        if (selectedObject != null) {
-            console.log("selected object is " + selectedObject.name);
-        } else {
-            console.log("selected object is null");
-        }
-    }, [selectedObject]);
+        console.log(galaxy);
+    }, [])
 
-    const updateSelectedObject = (object) => {
-        let foundObject = factory.findObject(galaxy.systems, object);
-        setSelectedObject(foundObject);
-    }
 
     return (
-        <div id="root-container" className="container-fluid">
-            <Navbar />
-            <div className="row align-items-start text-center h-100"> 
-                <CreatePanel data={galaxy} selected={selectedObject} callback={addNewChild} collapsed={isCreatePanelCollapsed}/>
-                <ChildrenPanel data={galaxy} selected={selectedObject} setSelected={updateSelectedObject} collapsed={isChildrenPanelCollapsed} />
-
-                <div className="closebtn left"><a onClick={isCreatePanelCollapsed ? toggleChildrenPanel : toggleCreatePanel}>&equiv;</a></div>
-                   <div className="button-left side-button"><button id="create" type="button" className="btn btn-outline-primary" onClick={toggleCreatePanel}>Create</button></div>
-
-                    <MainColumn data={galaxy} selected={selectedObject} />
-
-                    <div className="button-right side-button"><button id="save" type="button" className="btn btn-outline-danger">Save</button></div>
-
-                <div className="closebtn right"><a onClick={toggleInfoPanel}>&equiv;</a></div>
-                <InfoPanel selected={selectedObject} collapsed={isInfoPanelCollapsed} />
-            </div>
-        </div>
+        <SystemView system={galaxy.systems ? galaxy.systems[0] : {}}/>
     );
 }
 
--- src/ChildrenPanel.js
+++ src/ChildrenPanel.js
@@ -1,34 +1,32 @@
 import React from 'react';
 
 function ChildrenPanel(props) {
-    const galaxy = props.data;
+    const system = props.data;
     return (
         <section id="children-panel" className="col-md-2 px-0 text-light" style={{ transform: props.collapsed ? 'translateX(-100%)' : 'translateX(0)' }}>
             <div id="children-panel-content" className="collapse-horizontal">
                 <h1>Overview</h1>
                 {
-                    galaxy.systems.length > 0 ? (
+                    system.planets ? (
                         <nav className="container text-start">
                             {
-                                galaxy.systems.map((system) => (
-                                    <ul className="star">
-                                        <li key={system.name} className={`${props.selected && props.selected.name === system.name ? 'selected' : ''}`} onClick={() => {props.setSelected(system.name)}}>{system.name}</li>
-                                        <ul className="planet">
-                                            {system.planets.map((planet) => (
-                                            <React.Fragment>
-                                                <li key={planet.name} className={`${props.selected && props.selected.name === planet.name ? 'selected' : ''}`} onClick={() => {props.setSelected(planet.name)}}>{planet.name}</li>
-                                                {planet.moons && planet.moons.length > 0 && (
-                                                <ul className="moon">
-                                                    {planet.moons.map((moon) => (
-                                                    <li key={moon.name} className={`${props.selected && props.selected.name === moon.name ? 'selected' : ''}`} onClick={() => {props.setSelected(moon.name)}}>{moon.name}</li>
-                                                    ))}
-                                                </ul>
-                                                )}
-                                            </React.Fragment>
-                                            ))}
-                                        </ul>
+                                <ul className="star">
+                                    <li key={system.name} className={`${props.selected && props.selected.name === system.name ? 'selected' : ''}`} onClick={() => {props.setSelected(system.name)}}>{system.name}</li>
+                                    <ul className="planet">
+                                        {system.planets.map((planet) => (
+                                        <React.Fragment>
+                                            <li key={planet.name} className={`${props.selected && props.selected.name === planet.name ? 'selected' : ''}`} onClick={() => {props.setSelected(planet.name)}}>{planet.name}</li>
+                                            {planet.moons && planet.moons.length > 0 && (
+                                            <ul className="moon">
+                                                {planet.moons.map((moon) => (
+                                                <li key={moon.name} className={`${props.selected && props.selected.name === moon.name ? 'selected' : ''}`} onClick={() => {props.setSelected(moon.name)}}>{moon.name}</li>
+                                                ))}
+                                            </ul>
+                                            )}
+                                        </React.Fragment>
+                                        ))}
                                     </ul>
-                                ))
+                                </ul>
                             }
                         </nav>
                     ) : <p>Nothing to display.
