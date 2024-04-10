36297127e0653c3fef115807b71423dad4e4395b
retribyte <car100@students.uwf.edu>
2024-04-09 20:08:14 -0500

    Canvas uses math functions to scale long-distances

--- src/SystemCanvas.js
+++ src/SystemCanvas.js
@@ -7,14 +7,18 @@ function SystemCanvas(props) {
         var canvas = document.getElementById("solar-system-canvas");
         const width = canvas.width;
         const height = canvas.height;
-        const KM_CONVERSION_FACTOR_STAR = 1/20000;  // Radius:   10000 km = 1px
-        const KM_CONVERSION_FACTOR_OTHER = 1/1000;  // Radius:    1000 km = 1px
-        const AU_CONVERSION_FACTOR = 100;            // Distance:    50 km = 1px
+        
+        const GLOBAL_SCALE                  = 1;
+        const KM_CONVERSION_FACTOR_STAR     = 1 / 20000;    // Radius:   10000 km = 1px
+        const KM_CONVERSION_FACTOR_OTHER    = 1 / 1000;     // Radius:    1000 km = 1px
+        const AU_CONVERSION_FACTOR          = 150;          // Distance:   150 km = 1px
+        const BASE_CONVERT_MULTIPLIER       = 1 / Math.log(1.25);
 
         // Grab the Canvas context
         // ALWAYS clear it before re-rendering!!!
         var ctx = canvas.getContext("2d");
         ctx.clearRect(0, 0, width, height); // Don't remove
+        ctx.scale(GLOBAL_SCALE, GLOBAL_SCALE);
 
         console.log("data is", system);
 
@@ -41,10 +45,10 @@ function SystemCanvas(props) {
         const starOffset = starRadius * 2 + 20;
         for (const planet of system.planets) {
             console.log("Rendering planet:", planet.name);
-            let drawDistance = planet.distance * AU_CONVERSION_FACTOR + starOffset;
+            let drawDistance = Math.log(planet.distance + 1) * AU_CONVERSION_FACTOR + starOffset;
 
             ctx.beginPath();
-            const planetRadius = planet.size * KM_CONVERSION_FACTOR_OTHER;
+            const planetRadius = Math.log(planet.size * KM_CONVERSION_FACTOR_OTHER) * BASE_CONVERT_MULTIPLIER;
             const planetColor  = planet.type == 'terrestrial' ? 'brown' : 'cyan';
 
             ctx.arc(width/2, drawDistance, planetRadius, 0, 2 * Math.PI);
@@ -57,7 +61,7 @@ function SystemCanvas(props) {
     });
 
     return (
-        <canvas id="solar-system-canvas" width="500" height="500" style={{border: "5px solid white"}}>
+        <canvas id="solar-system-canvas" width="1000" height="1000" style={{border: "5px solid white"}}>
 
         </canvas>
     );
