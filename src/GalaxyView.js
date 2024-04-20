14e174a9018a831f2db8c3fa1a7faa36659d855c
retribyte <treyroemer@gmail.com>
2024-04-20 00:18:39 -0500

    Adjustments to GalaxyView structure. Rename some files.

--- src/GalaxyView.js
+++ src/GalaxyView.js
@@ -1,37 +1,40 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import LeftPanel from './LeftGalaxy';
-import RightPanel from './RightGalaxy'; 
+import GalaxyOverview from './GalaxyOverview';
+import RightPanel from './RightGalaxy';
 import GalaxyAdd from './GalaxyAdd';
 
 function GalaxyView() {
-  const [leftPanelOpen, setLeftPanelOpen] = useState(true);
-  const [isGalaxyAddOpen, setIsGalaxyAddOpen] = useState(false);
+    const [isChildrenPanelCollapsed, setChildrenPanelCollapsed] = useState(false);
+    const [isCreatePanelCollapsed, setCreatePanelCollapsed] = useState(true);
+    const [isInfoPanelCollapsed, setInfoPanelCollapsed] = useState(false);
 
-  const toggleLeftPanel = () => {
-    setLeftPanelOpen(!leftPanelOpen);
-  };
+    const [isLeftPanelCollapsed, setLeftPanelCollapsed] = useState(true);
+    const [isGalaxyAddCollapsed, setIsGalaxyAddCollapsed] = useState(false);
 
-  const toggleGalaxyAdd = () => {
-    setIsGalaxyAddOpen(!isGalaxyAddOpen);
-  };
+    const toggleLeftPanel = () => {
+        setLeftPanelCollapsed(!isLeftPanelCollapsed);
+    };
 
+    const toggleGalaxyAdd = () => {
+        setIsGalaxyAddCollapsed(!isGalaxyAddCollapsed);
+    };
 
-  return (
-    <div className="galaxy-page-container">
-      <LeftPanel isOpen={leftPanelOpen} togglePanel={toggleLeftPanel} />
-      <div className="galaxy-panels">
-        <button className="toggle-button" onClick={toggleLeftPanel}>
-          Create
-        </button>
+    return (
+        <div className="galaxy-page-container">
+            <GalaxyOverview collapsed={isChildrenPanelCollapsed} />
+            <div className="galaxy-panels">
+                <button className="toggle-button" onClick={toggleLeftPanel}>
+                    Create
+                </button>
 
-      </div>
-      <RightPanel isOpen={true} /> 
-      <button id="save" className="save-button" onClick={() => console.log("Save clicked")}>Save</button>
-      <button id="load" className="load-button" onClick={() => console.log("Load clicked")}>Load</button>
-      <GalaxyAdd isOpen={isGalaxyAddOpen} onClose={toggleGalaxyAdd} />
-    </div>
-  );
+            </div>
+            <RightPanel isOpen={true} />
+            <button id="save" className="save-button" onClick={() => console.log("Save clicked")}>Save</button>
+            <button id="load" className="load-button" onClick={() => console.log("Load clicked")}>Load</button>
+            <GalaxyAdd isOpen={isGalaxyAddCollapsed} onClose={toggleGalaxyAdd} />
+        </div>
+    );
 }
 
 export default GalaxyView;
